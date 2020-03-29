3c1440507bcee441fa7f769204c50875c064abdb
sjoseph1848 <sjoseph1848@gmail.com>
2020-03-29 12:16:03 -0500

    setup main page for groceries

--- App.js
+++ App.js
@@ -13,6 +13,21 @@ export default function App() {
   const [groceries, setGroceries] = useState([]);
   const [isAddGroceries, setIsAddGroceries] = useState(false);
 
+  const addGroceryHandler = (groceryItem) => {
+    // eslint-disable-next-line no-sparse-arrays
+    setGroceries((currentGroceries) => [
+      ...groceries,
+      {id: Math.random().toString(), value: groceryItem},
+    ]);
+    setIsAddGroceries(false);
+  };
+
+  const removeGroceryHandler = (groceryId) => {
+    setGroceries((currentGroceries) => {
+      return currentGroceries.filter((grocery) => grocery.id !== groceryId);
+    });
+  };
+
   return (
     <View style={styles.screen}>
       <Button title="Add Groceries" onPress={() => setIsAddGroceries(true)} />
